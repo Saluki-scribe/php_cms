a3f3bb2197e6358e1dae1d75c46885152ced6be4
Saluki-scribe <tara@nelsonwriting.com>
2018-06-05 11:19:37 -0400

    Added styling and back link to show.php

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -4,10 +4,6 @@
 
 $id = $_GET['id'] ?? '1'; //PHP > 7.0
 
-echo h($id);
+echo h('Subject Id: ' . $id);
 
 ?>
-
-<a href="show.php?name=<?php echo u('John Doe');  ?>">Link</a><br />
-<a href="show.php?company=<?php echo u('Widgets&More');  ?>">Link</a><br />
-<a href="show.php?query=<?php echo u('!#*?');  ?>">Link</a><br />
