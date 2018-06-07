abb5488ef19c97cb7d15127781098c00cb4beebc
Saluki-scribe <tara@nelsonwriting.com>
2018-06-07 15:23:03 -0400

    Added phpinfo.php to view data. Added content div and backlink to new.php

--- public/staff/subjects/new.php
+++ public/staff/subjects/new.php
@@ -8,7 +8,16 @@ if($test == '404') {
     error_404();
 } else if($test == '500') {
     error_500();
-} else {
-    echo 'No error';
+} else if($test == 'redirect') {
+    redirect_to(url_for('/staff/subjects/index.php'));
 }
 ?>
+
+<?php $page_title = 'Create Subject'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+
+    <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
+
+</div>
