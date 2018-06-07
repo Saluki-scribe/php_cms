f5162f58fd252d0a5072961be9d8d0fe03a93117
Saluki-scribe <tara@nelsonwriting.com>
2018-06-07 15:32:30 -0400

    Added form inputs.

--- public/staff/subjects/new.php
+++ public/staff/subjects/new.php
@@ -24,9 +24,32 @@ if($test == '404') {
         <h1>Create Subject</h1>
 
         <form action="" method="post">
-        
+            <dl>
+                <dt>Menu Name</dt>
+                <dd><input type="text" name="menu_name" value="" /></dd>
+            </dl>
+            <dl>
+                <dt>Position</dt>
+                <dd>
+                    <select name="position">
+                        <option value="1">1</option>
+                    </select>
+                </dd>
+            </dl>
+            <dl>
+                <dt>Visible</dt>
+                <dd>
+                    <input type="hidden" name="visible" value="0"/>
+                    <input type="checkbox" name="visible" value="1"/>
+                </dd>
+            </dl>
+            <div id="operations">
+                <input type="submit" value="Create Subject" />
+            </div>
         </form>
 
     </div>
 
 </div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
