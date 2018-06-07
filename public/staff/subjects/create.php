83ce556e3fe409e33b01f99ccc1fb34870765348
Saluki-scribe <tara@nelsonwriting.com>
2018-06-07 19:01:59 -0400

    Added conditional to create.php to check if form was submitted and if not, redirects back to form.

--- public/staff/subjects/create.php
+++ public/staff/subjects/create.php
@@ -1,5 +1,9 @@
 <?php 
 
+require_once('../../../private/initialize.php'); 
+
+if(is_post_request()) {
+
 // Handle form values sent by new.php
 
 $menu_name = $_POST['menu_name'] ?? '';
@@ -10,5 +14,11 @@ echo "Form parameters<br />";
 echo "Menu name: " . $menu_name . "<br />";
 echo "Position: " . $position . "<br />";
 echo "Visible: " . $visible . "<br />";
+
+} else {
+    redirect_to(url_for('/staff/subjects/new.php'));
+}
+
+
 
 ?>
