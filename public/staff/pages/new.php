5cd7b268934e9d3189af691cecedf5aa74b98218
Saluki-scribe <tara@nelsonwriting.com>
2018-06-08 11:22:46 -0400

    Added new.php to staff/pages directory.

---
+++ public/staff/pages/new.php
@@ -0,0 +1,15 @@
+<?php 
+
+require_once('../../../private/initialize.php'); 
+
+?>
+
+<?php $page_title = 'Create Page'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+
+
+
+
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
