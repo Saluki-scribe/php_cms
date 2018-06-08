d0efaf427a24fcc17e00d403be7b6d7eee7b5250
Saluki-scribe <tara@nelsonwriting.com>
2018-06-08 12:23:56 -0400

    Finalized page and subject creation forms

--- public/staff/subjects/new.php
+++ public/staff/subjects/new.php
@@ -2,15 +2,24 @@
 
 require_once('../../../private/initialize.php'); 
 
-$test = $_GET['test'] ?? '';
+$menu_name = '';
+$position = '';
+$visible = '';
 
-if($test == '404') {
-    error_404();
-} else if($test == '500') {
-    error_500();
-} else if($test == 'redirect') {
-    redirect_to(url_for('/staff/subjects/index.php'));
-}
+if(is_post_request()) {
+
+    // Handle form values sent by new.php
+    
+    $menu_name = $_POST['menu_name'] ?? '';
+    $position = $_POST['position'] ?? '';
+    $visible = $_POST['visible'] ?? '';
+    
+    echo "Form parameters<br />";
+    echo "Menu name: " . $menu_name . "<br />";
+    echo "Position: " . $position . "<br />";
+    echo "Visible: " . $visible . "<br />";
+    
+} 
 ?>
 
 <?php $page_title = 'Create Subject'; ?>
@@ -23,16 +32,16 @@ if($test == '404') {
     <div class="subject new">
         <h1>Create Subject</h1>
 
-        <form action="<?php echo url_for('/staff/subjects/create.php'); ?>" method="post">
+        <form action="<?php echo url_for('/staff/subjects/new.php'); ?>" method="post">
             <dl>
                 <dt>Menu Name</dt>
-                <dd><input type="text" name="menu_name" value="" /></dd>
+                <dd><input type="text" name="menu_name" value="<?php echo h($menu_name); ?>" /></dd>
             </dl>
             <dl>
                 <dt>Position</dt>
                 <dd>
                     <select name="position">
-                        <option value="1">1</option>
+                        <option value="1" <?php if($position == "1") { echo " selected"; } ?>>1</option>
                     </select>
                 </dd>
             </dl>
@@ -40,7 +49,7 @@ if($test == '404') {
                 <dt>Visible</dt>
                 <dd>
                     <input type="hidden" name="visible" value="0"/>
-                    <input type="checkbox" name="visible" value="1"/>
+                    <input type="checkbox" name="visible" value="1" <?php if($visible == "1") {echo " checked"; } ?>/>
                 </dd>
             </dl>
             <div id="operations">
