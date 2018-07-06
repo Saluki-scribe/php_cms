d0c14c436d29775e3ad1b94240dd7fd1ff962674
Saluki-scribe <tara@nelsonwriting.com>
2018-07-05 21:28:42 -0400

    Created function, insert_subject to replace raw code for creating records

--- public/staff/subjects/create.php
+++ public/staff/subjects/create.php
@@ -10,24 +10,9 @@ $menu_name = $_POST['menu_name'] ?? '';
 $position = $_POST['position'] ?? '';
 $visible = $_POST['visible'] ?? '';
 
-$sql = "INSERT INTO subjects ";
-$sql .= "(menu_name, position, visible) ";
-$sql .= "VALUES (";
-$sql .= "'" . $menu_name . "',";
-$sql .= "'" . $position . "',";
-$sql .= "'" . $visible . "'";
-$sql .= ")";
-$result = mysqli_query($db, $sql);
-// For INSERT statements, $result is true/false
-if($result) {
-    $new_id = mysqli_insert_id($db);
-    redirect_to(url_for('/staff/subjects/show.php?id=' . $new_id));
-} else {
-    //INSERT failed
-    echo mysqli_error($db);
-    db_disconnect($db);
-    exit;
-}
+$result = insert_subject($menu_name, $position, $visible);
+$new_id = mysqli_insert_id($db);
+redirect_to(url_for('/staff/subjects/show.php?id=' . $new_id));
 
 } else {
     redirect_to(url_for('/staff/subjects/new.php'));
