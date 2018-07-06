2d7da9d1404105e1d5d06dae0eb1fc3f50126134
Saluki-scribe <tara@nelsonwriting.com>
2018-07-06 17:00:37 -0400

    Created validation_functions.php and created several validation functions

--- private/validation_functions.php
+++ private/validation_functions.php
@@ -3,7 +3,55 @@
 // is_blank('abcd')
 // * validate data presence
 // * uses trime() so empty spaces don't count
-function is_blank($vlaue)
+function is_blank($vlaue) {
+    return !isset($value) || trim($value) === '';
+}
+
+function has_presence($value) {
+    return !is_blank($value);
+}
+
+function has_length_greater_than($value, $min) {
+    $length = strlen($value);
+    return $length > $min;
+}
+
+function has_length_less_than($value, $max) {
+    $length = strlen($value);
+    return $length < $max;
+}
+
+function has_length_exactly($value, $exact) {
+    $length = strlen($value);
+    return $length == $exact;
+}
+
+function has_length($value, $options) {
+    if(isset($options['min']) && !has_length_greater_than($value, $options['min'] - 1)) {
+        return false;
+    } elseif(isset($options['max']) && !has_length_less_than($value, $options['exact'])) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function has_inclusion_of($value, $set) {
+    return in_array($value, $set);
+}
+
+function has_exclusion_of($value, $set) {
+    return !in_array($value, $set);
+}
+
+function has_string($value, $required_string) {
+    return strpos($value, $required_string) !== false;
+}
+
+function has_valid_email_format($value) {
+    $email_regex = '/\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\Z/i';
+    return preg_match($email_regex, $value) === 1;
+}
 
 
 ?>
