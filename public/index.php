53b0a13efe9161ad38a729cd72cb71dae38553fc
Saluki-scribe <tara@nelsonwriting.com>
2018-07-10 23:18:11 -0400

    Updated files

--- public/index.php
+++ public/index.php
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Globe Bank</title>
-</head>
-<body>
-    
-    <h1>Globe Bank Coming Soon</h1>
+<?php require_once('../private/initialize.php'); ?>
 
-</body>
-</html>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+<div id="main">
+
+    <div id="page">
+
+    </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
