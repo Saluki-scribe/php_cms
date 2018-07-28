8564bea77cb5052f0357e74571c2ecf7cf70fd09
Saluki-scribe <tara@nelsonwriting.com>
2018-07-27 22:39:44 -0400

    Created conditional statement in index.php so that if page is clicked, page's content shows up

--- public/index.php
+++ public/index.php
@@ -23,12 +23,19 @@
     <div id="page">
 
         <?php 
-            // Show the homepage
-            // The homepage content could:
-            // * be static content (here or in a shared file)
-            // * show the first page from the nav
-            // * be in the database but add code to hide in the nav
-            include(SHARED_PATH . '/static_homepage.php'); 
+            if(isset($page)) {
+                // show page from database
+                // TODO add html escaping back in
+                echo $page['content'];
+            } else {
+                // Show the homepage
+                // The homepage content could:
+                // * be static content (here or in a shared file)
+                // * show the first page from the nav
+                // * be in the database but add code to hide in the nav
+                include(SHARED_PATH . '/static_homepage.php'); 
+            }
+            
         ?>
 
     </div>
