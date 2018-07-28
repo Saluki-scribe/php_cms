66ca72f7e910fc56c7e5b1baf931b25672442d24
Saluki-scribe <tara@nelsonwriting.com>
2018-07-27 22:45:23 -0400

    Added conditional to public_navigation.php pages while loop to give the 'selected' css class to whichever page is click in nav

--- private/shared/public_navigation.php
+++ private/shared/public_navigation.php
@@ -10,7 +10,7 @@
         <?php $nav_pages = find_pages_by_subject_id($nav_subject['id']); ?>
           <ul class="pages">
             <?php while($nav_page = mysqli_fetch_assoc($nav_pages)) { ?>
-              <li>
+              <li class="<?php if($nav_page['id'] == $page_id) { echo 'selected';} ?>">
                 <a href="<?php echo url_for('index.php?id=' . h(u($nav_page['id']))); ?>">
                   <?php echo h($nav_page['menu_name']); ?>
                 </a>
